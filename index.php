a07e81cbdd6ca7c15c32813139b38bfcd2a1ff5c
Luc Flynn <lucnmsu@gmail.com>
2017-04-12 08:22:05 -0600

    added css file

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
+	<!-- The head tag contains meta data about this html doc -->
 	<head>
-		<meta charset="utb-18 /"/>
+		<!-- Always need a meta charset -->
+		<meta charset="utf-8 "/>
+		<!-- Below is how you link to your css style sheet file -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
